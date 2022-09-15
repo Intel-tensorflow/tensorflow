676652d7f0f4e15d9115c79aaaa5db47fe525511
Erxin Shang <erxin.shang@intel.com>
2022-09-15 10:25:47 +0800

    Add validation script for sanity tests and wheel size

---
+++ tensorflow/tools/ci_build/windows/jenkins_ci/validation/sanity_tests.py
@@ -0,0 +1,20 @@
+"""
+This file contains the sanity tests need to be run after we build a
+TensorFlow wheel.
+
+Check the BKM to get more information
+https://wiki.ith.intel.com/display/intelnervana/BKM%3A+Public+CI+Monitoring+on+Windows#BKM:PublicCIMonitoringonWindows-HowtoVerifyifapackageisinstalledandruntestsonit
+"""
+
+import tensorflow as tf
+
+print("Validate keras")
+assert "_v2.keras" in tf.keras.__name__, "Test _v2.keras in tf.keras.__name__"
+
+print("Validate array shape after adding")
+t1 = tf.constant([1, 2, 3, 4])
+t2 = tf.constant([5, 6, 7, 8])
+assert tf.add(t1, t2).shape == (4,), "Test array shape after adding two arrays"
+
+print("Validate tf.estimator")
+assert "_v2.estimator" in tf.estimator.__name__, "Test _v2.estimator in tf.estimator.__name__"
