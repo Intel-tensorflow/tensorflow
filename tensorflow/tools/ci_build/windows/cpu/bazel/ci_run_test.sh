9e6bd384582a32adbb557fad5c5a865ebfc43a8a
mraunak <83710963+mraunak@users.noreply.github.com>
2022-11-26 18:38:21 -0800

    Update ci_run_test.sh

--- tensorflow/tools/ci_build/windows/cpu/bazel/ci_run_test.sh
+++ tensorflow/tools/ci_build/windows/cpu/bazel/ci_run_test.sh
@@ -178,7 +178,6 @@ bazel --windows_enable_symlinks test \
   --config=opt \
   -k --test_output=errors \
   --test_tag_filters=-no_windows,-no_oss,-gpu,-tpu,-v1only \
-  --copt=/d2ReducedOptimizeHugeFunctions,--host_copt=/d2ReducedOptimizeHugeFunctions \
   --discard_analysis_cache \
   --test_size_filters=small,medium --jobs=16 --test_timeout=300,450,1200,3600 --verbose_failures \
   --flaky_test_attempts=3 \
