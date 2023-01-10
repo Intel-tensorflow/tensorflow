d23342a4a6a5205b0cfe76004b7de49b74236608
Mayank Raunak <mayank.kumar.raunak@intel.com>
2023-01-10 15:50:39 -0800

    separate testcases

--- tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
@@ -176,8 +176,35 @@ bazel test --announce_rc --config=opt -k --test_output=errors \
   --define=no_tensorflow_py_deps=true --test_lang_filters=py \
   --test_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu,-v1only \
   --build_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu --build_tests_only \
-  --test_size_filters=small,medium \
+  --test_size_filters=small \
   --jobs="${N_JOBS}" --test_timeout="300,450,1200,3600" \
   --flaky_test_attempts=3 \
   --output_filter=^$ \
   ${TEST_TARGET}
+
+MYTFWS_ROOT=${WORKSPACE:-"C:/Users/mlp_admin"} # keep the tensorflow git repo clone under here as tensorflow subdir
+MYTFWS_ROOT=`cygpath -m $MYTFWS_ROOT`
+export MYTFWS_ROOT="$MYTFWS_ROOT"
+export MYTFWS_NAME="tensorflow"
+export MYTFWS="${MYTFWS_ROOT}/${MYTFWS_NAME}"
+
+cd $MYTFWS
+
+
+TEST_TARGET_1="//tensorflow/... -//tensorflow/java/... -//tensorflow/lite/... -//tensorflow/compiler/xla/python/tpu_driver/... -//tensorflow/compiler/... -//tensorflow/go/... -//tensorflow/js/... -//tensorflow/python/distribute/... -//tensorflow/python/... -//tensorflow/core/distributed_runtime/... -//tensorflow/tsl/distributed_runtime/..."
+
+bazel  test \
+  --action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS} \
+  --experimental_cc_shared_library --enable_runfiles --nodistinct_host_configuration \
+  --dynamic_mode=off --config=xla --config=short_logs --announce_rc \
+  --build_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu --build_tests_only --config=monolithic \
+  --config=opt \
+  -k --test_output=errors \
+  --test_tag_filters=-no_windows,-no_oss,-gpu,-tpu,-v1only \
+  --discard_analysis_cache \
+  --test_size_filters=small --jobs=16 --test_timeout=300,450,1200,3600 --verbose_failures \
+  --flaky_test_attempts=3 \
+  ${POSITIONAL_ARGS[@]} \
+  -- ${TEST_TARGET_1} 
+
+
