4758b3ef8063d551a7d653fa8b8056b9a5c15bed
Mayank Raunak <mayank.kumar.raunak@intel.com>
2023-01-12 13:21:51 -0800

    test_pass

--- tensorflow/tools/ci_build/windows/cpu/pip/ci_run_build_test.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/ci_run_build_test.sh
@@ -113,8 +113,8 @@ if [ "${PYTHON_VERSION}" == 311 ]
 then
   path_level=$(dirname ${MYTFWS_ROOT})
 
-  cp "${path_level}"/h5py_whl/artifact/h5py-3.7.0-cp311-cp311-win_amd64.whl ${MYTFWS_ARTIFACT}
-  python -m pip install ${MYTFWS_ARTIFACT}/h5py-3.7.0-cp311-cp311-win_amd64.whl
+  cp "${path_level}"/h5py_whl/artifact/h5py-3.7.0-cp311-cp311-win_amd64.whl ${MYTFWS_ROOT}
+  python -m pip install ${MYTFWS_ROOT}/h5py-3.7.0-cp311-cp311-win_amd64.whl
     
 fi
 python -m pip install -r $MYTFWS/tensorflow/tools/ci_build/release/requirements_common.txt
@@ -171,6 +171,8 @@ cd $MYTFWS_ROOT
 if [[ "$TF_NIGHTLY" = 1 ]] || [[ "$RELEASE_BUILD" = 1 ]]; then
   if [[ $build_ret_val -eq 0 ]]; then
     cp ${MYTFWS}/py_test_dir/*.whl ${MYTFWS_ARTIFACT}
+    cp "${path_level}"/h5py_whl/artifact/h5py-3.7.0-cp311-cp311-win_amd64.whl ${MYTFWS_ARTIFACT}
+    python -m pip install ${MYTFWS_ARTIFACT}/h5py-3.7.0-cp311-cp311-win_amd64.whl
     cp "${MYTFWS}"/run.log ${MYTFWS_ARTIFACT}/test_run_${PYTHON_VERSION}.log
   else
     # build failed just copy the log, mark log with py version.
