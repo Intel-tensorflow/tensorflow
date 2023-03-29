1c23afd40bd22be28dce277dad77735075e6ad87
mraunak <83710963+mraunak@users.noreply.github.com>
2023-03-29 10:29:34 -0700

    Update ci_xla_tests

--- tensorflow/tools/ci_build/windows/cpu/bazel/ci_xla_tests.sh
+++ tensorflow/tools/ci_build/windows/cpu/bazel/ci_xla_tests.sh
@@ -170,7 +170,18 @@ set +e   # Unset so script continues even if commands fail, this is needed to co
 N_JOBS="${NUMBER_OF_PROCESSORS}"
 
 # --config=release_cpu_windows 
-bazel >bazel --output_user_root=$MYTFWS test --config=dbg --config=opt -k \
+bazel --windows_enable_symlinks test \
+  --action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS} \
+  --experimental_cc_shared_library --enable_runfiles --nodistinct_host_configuration \
+  --dynamic_mode=off --config=xla --config=short_logs --announce_rc \
+  --build_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu --build_tests_only --config=monolithic \
+  --config=opt \
+  -k --test_output=errors \
+  --test_tag_filters=-no_windows,-no_oss,-gpu,-tpu,-v1only \
+  --discard_analysis_cache \
+  --test_timeout=300,450,1200,3600 --verbose_failures \
+  --flaky_test_attempts=3 \
+  ${POSITIONAL_ARGS[@]} \
   -- ${TEST_TARGET} \
   > run.log 2>&1
 
