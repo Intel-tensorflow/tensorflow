2a850f6b277cb95e2238c13712a641c72195719e
mraunak <83710963+mraunak@users.noreply.github.com>
2023-04-17 10:33:33 -0700

    Debugger

--- tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
@@ -34,7 +34,7 @@ set -e
 # Change into repository root.
 script_dir=$(dirname $0)
 cd ${script_dir%%tensorflow/tools/ci_build/windows/cpu/pip}.
-echo "${pwd}"
+echo "working"
 
 # Setting up the environment variables Bazel and ./configure needs
 source "tensorflow/tools/ci_build/windows/bazel/common_env.sh" \
@@ -181,7 +181,7 @@ bazel test --announce_rc --config=opt -k --test_output=errors \
   --jobs="${N_JOBS}" --test_timeout="300,450,1200,3600" \
   --flaky_test_attempts=3 \
   -- ${TEST_TARGET}
-
+echo "reached"
 MYTFWS_ROOT=${WORKSPACE:-"C:/Users/mlp_admin"} # keep the tensorflow git repo clone under here as tensorflow subdir
 MYTFWS_ROOT=`cygpath -m $MYTFWS_ROOT`
 export MYTFWS_ROOT="$MYTFWS_ROOT"
@@ -189,23 +189,8 @@ export MYTFWS_NAME="tensorflow"
 export MYTFWS="${MYTFWS_ROOT}/${MYTFWS_NAME}"
 
 cd $MYTFWS
+echo "${MYTFWS}"
 
-
-# ="//tensorflow/... -//tensorflow/java/... -//tensorflow/lite/... -//tensorflow/compiler/xla/python/tpu_driver/... -//tensorflow/compiler/... -//tensorflow/go/... -//tensorflow/js/... -//tensorflow/python/... -//tensorflow/core/distributed_runtime/... -//tensorflow/tsl/distributed_runtime/..."
-
-# bazel --windows_enable_symlinks test \
-#   --action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS} \
-#   --experimental_cc_shared_library --enable_runfiles --nodistinct_host_configuration \
-#   --dynamic_mode=off --config=xla --config=short_logs --announce_rc \
-#   --build_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu --build_tests_only --config=monolithic \
-#   --config=opt \
-#   -k --test_output=errors \
-#   --test_tag_filters=no_pip,-no_windows,-no_oss,-gpu,-tpu,-v1only \
-#   --discard_analysis_cache \
-#   --test_size_filters=small --jobs="${N_JOBS}" --test_timeout=300,450,1200,3600 --verbose_failures \
-#   --flaky_test_attempts=3 \
-#   ${POSITIONAL_ARGSTEST_TARGET_1[@]} \
-#   -- ${TEST_TARGET_1} 
 
   bazel --windows_enable_symlinks test \
   --action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS} --test_env=TF2_BEHAVIOR=1 \
@@ -219,5 +204,3 @@ cd $MYTFWS
   ${POSITIONAL_ARGS[@]} \
    -- //tensorflow/... -//tensorflow/java/... -//tensorflow/lite/... -//tensorflow/compiler/xla/python/tpu_driver/... -//tensorflow/compiler/... -//tensorflow/go/... -//tensorflow/js/... -//tensorflow/python/... -//tensorflow/core/distributed_runtime/... -//tensorflow/tsl/distributed_runtime/...
  
-
-
