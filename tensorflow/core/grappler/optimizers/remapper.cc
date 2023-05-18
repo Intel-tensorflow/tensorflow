3b009f4a2690b8f8b25ec48d49543f8d6c762f93
Gauri1 Deshpande <gauri1.deshpande@intel.com>
2023-05-18 16:58:51 -0700

    disable instancenorm fusion for onednn v3 till kernel support is added

--- tensorflow/core/grappler/optimizers/remapper.cc
+++ tensorflow/core/grappler/optimizers/remapper.cc
@@ -4528,6 +4528,7 @@ Status Remapper::Optimize(Cluster* cluster, const GrapplerItem& item,
         continue;
       }
 
+#if defined(ENABLE_ONEDNN_V2)
       // Remap ops that make up instancenorm followed by Relu or LeakyRelu
       // into _MklFusedInstanceNorm
       matched_nodes_map.clear();
@@ -4549,6 +4550,7 @@ Status Remapper::Optimize(Cluster* cluster, const GrapplerItem& item,
             &nodes_to_delete, false));
         continue;
       }
+#endif
     }
 
     // Remap MatMul + BiasAdd + gelu-subgraph
