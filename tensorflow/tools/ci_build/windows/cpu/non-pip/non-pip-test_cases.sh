790a395e696a1747237ae68534c6c6c72afa36e4
mraunak <83710963+mraunak@users.noreply.github.com>
2023-06-12 01:08:15 -0700

    Update non-pip-test_cases.sh

--- tensorflow/tools/ci_build/windows/cpu/non-pip/non-pip-test_cases.sh
+++ tensorflow/tools/ci_build/windows/cpu/non-pip/non-pip-test_cases.sh
@@ -169,18 +169,19 @@ set +e   # Unset so script continues even if commands fail, this is needed to co
 # NUMBER_OF_PROCESSORS is predefined on Windows
 N_JOBS="${NUMBER_OF_PROCESSORS}"
 
-# --config=release_cpu_windows 
+# --config=release_cpu_windows
 bazel --windows_enable_symlinks test \
   --action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS} \
   --experimental_cc_shared_library --enable_runfiles --nodistinct_host_configuration \
   --dynamic_mode=off --config=xla --config=short_logs --announce_rc \
-  --test_lang_filters=cc,py \
-  --build_tag_filters=-no_pip,-no_windows,-excluded_windows,-oss_serial,-gpu,-tpu --build_tests_only --config=monolithic \
+  --build_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu --build_tests_only --config=monolithic \
   --config=opt \
   -k --test_output=errors \
-  --test_tag_filters=-no_pip,-no_windows,-excluded_windows,-oss_serial,-gpu,-tpu \
+  --test_lang_filters=cc,py \
+  --test_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu \
   --discard_analysis_cache \
   --test_size_filters=small,medium --jobs=16 --test_timeout=300,450,1200,3600 --verbose_failures \
+  --flaky_test_attempts=3 \
   ${POSITIONAL_ARGS[@]} \
   -- ${TEST_TARGET} \
   > run.log 2>&1
