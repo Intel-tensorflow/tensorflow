d0d83ad62383285b51ae809dfa5a4855d5e6f52c
mraunak <83710963+mraunak@users.noreply.github.com>
2023-08-14 17:32:55 -0700

    Update build_tf_windows.sh

--- tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
@@ -101,14 +101,11 @@ while [[ $# -gt 0 ]]; do
   shift
 done
 
-if [[ "$RELEASE_BUILD" == 1 ]]; then
+if [[ "$RELEASE_BUILD" == 1 || "$RELEASE_BUILD" == 0 ]]; then
   # Overriding eigen strong inline speeds up the compiling of conv_grad_ops_3d.cc and conv_ops_3d.cc
   # by 20 minutes. See https://github.com/tensorflow/tensorflow/issues/10521
   # Because this hurts the performance of TF, we don't override it in release build.
   export TF_OVERRIDE_EIGEN_STRONG_INLINE=1
-else
-  export TF_OVERRIDE_EIGEN_STRONG_INLINE=1
-fi
 
 if [[ "$TF_NIGHTLY" == 1 ]]; then
   if [[ ${PROJECT_NAME} == *"2.0_preview"* ]]; then
