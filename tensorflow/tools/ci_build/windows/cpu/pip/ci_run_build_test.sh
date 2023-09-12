401fb5428eb85e76f8d1eff3dfb89c5919c2af6c
mraunak <83710963+mraunak@users.noreply.github.com>
2023-09-12 12:18:10 -0700

    Update ci_run_build_test.sh

--- tensorflow/tools/ci_build/windows/cpu/pip/ci_run_build_test.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/ci_run_build_test.sh
@@ -141,7 +141,7 @@ set +e   # Unset so script continues even if commands fail, this is needed to co
 cd $MYTFWS
 
 bash "${MYTFWS}"/tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh \
-   --extra_build_flags "--action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XBF_ARGS} --repo_env=TF_PYTHON_VERSION=${TF_PYTHON_VERSION}" \
+   --extra_build_flags "--action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XBF_ARGS} --repo_env=TF_PYTHON_VERSION=${TF_PYTHON_VERSION} --test_env=TF_ENABLE_ONEDNN_OPTS=1" \
    --extra_test_flags "--action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS}" \
    ${POSITIONAL_ARGS[@]}  > run.log 2>&1
 
