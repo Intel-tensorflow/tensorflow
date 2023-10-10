d8c0c143e9a667f3e7ebadbf17c0fbf2e44ee16f
mraunak <83710963+mraunak@users.noreply.github.com>
2023-10-10 14:34:15 -0700

    Update ci_run_test.sh

--- tensorflow/tools/ci_build/windows/cpu/bazel/ci_run_test.sh
+++ tensorflow/tools/ci_build/windows/cpu/bazel/ci_run_test.sh
@@ -170,14 +170,14 @@ set +e   # Unset so script continues even if commands fail, this is needed to co
 
 # NUMBER_OF_PROCESSORS is predefined on Windows
 N_JOBS="${NUMBER_OF_PROCESSORS}"
-
+start-stop-daemon -b -n portserver.py -a /usr/local/bin/python3 -S -- /usr/local/bin/portserver.py
 # --config=release_cpu_windows 
 bazel --windows_enable_symlinks test \
   --action_env=TEMP=${TMP} --action_env=TMP=${TMP} ${XTF_ARGS} \
   --experimental_cc_shared_library --enable_runfiles --nodistinct_host_configuration \
   --dynamic_mode=off --config=xla --config=short_logs --announce_rc \
   --build_tag_filters=-no_pip,-no_windows,-no_oss,-gpu,-tpu --build_tests_only --config=monolithic \
-  --config=opt \
+  --config=opt --test_env=PORTSERVER_ADDRESS=@unittest-portserver \
   --repo_env=TF_PYTHON_VERSION=${TF_PYTHON_VERSION} \
   -k --test_output=errors \
   --test_tag_filters=-no_windows,-no_oss,-gpu,-tpu \
