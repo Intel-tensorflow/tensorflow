b90cfb561ee73d70e5a92d75df1bd8d89cd39c82
mraunak <83710963+mraunak@users.noreply.github.com>
2024-02-07 12:03:23 -0800

    Update ci_run_test.sh

--- tensorflow/tools/ci_build/windows/cpu/bazel/ci_run_test.sh
+++ tensorflow/tools/ci_build/windows/cpu/bazel/ci_run_test.sh
@@ -69,7 +69,7 @@ export TEST_TARGET=${TEST_TARGET:-"//tensorflow/..."}
 export MSYS_LOCATION='C:/msys64'
 export GIT_LOCATION='C:/Program Files/Git'
 export JAVA_LOCATION='C:/Program Files/Eclipse Adoptium/jdk-11.0.14.101-hotspot'
-export VS_LOCATION='C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools'
+export VS_LOCATION='C:/Program Files/Microsoft Visual Studio/2022/Community'
 export NATIVE_PYTHON_LOCATION="C:/Python${PYTHON_VERSION}"
 
 
