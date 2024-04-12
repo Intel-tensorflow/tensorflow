ef8f8c8185573bdcc258764c5e07af44a630ec0b
mraunak <83710963+mraunak@users.noreply.github.com>
2024-04-11 22:42:20 -0700

    Update ci_run_build_test.sh

--- tensorflow/tools/ci_build/windows/cpu/pip/ci_run_build_test.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/ci_run_build_test.sh
@@ -41,7 +41,7 @@ done
 #SCRIPT_ARGS=${POSITIONAL_ARGS[@]}
 
 # bazelisk (renamed as bazel) is kept in C:\Tools
-export PATH=/c/Tools/bazel:/c/Program\ Files/Git:/c/Program\ Files/Git/cmd:/c/msys64:/c/msys64/usr/bin:/c/Windows/system32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/OpenSSH:/c/Program\ Files/LLVM/bin
+export PATH=/c/Tools/bazel:/c/Program\ Files/Git:/c/Program\ Files/Git/cmd:/c/msys64:/c/msys64/usr/bin:/c/Windows/system32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/OpenSSH:/c/LLVM/bin
 
 # Environment variables to be set by Jenkins before calling this script
 
@@ -71,7 +71,7 @@ export JAVA_LOCATION='C:/Program Files/Eclipse Adoptium/jdk-11.0.14.101-hotspot'
 export VS_LOCATION='C:/Program Files/Microsoft Visual Studio/2022/Community'
 export NATIVE_PYTHON_LOCATION="C:/Python${PYTHON_VERSION}"
 export BAZEL_VC_FULL_VERSION=14.39.33519
-export LLVM_LOCATION='C:/Program Files/LLVM'
+export LLVM_LOCATION='C:/LLVM'
 
 
 echo "*** *** hostname is $(hostname) *** ***"
